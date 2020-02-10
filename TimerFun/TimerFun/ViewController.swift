8bc228cfdb71688e717276fd552de309b33a11e7
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-02-10 13:02:44 -0500

    initial commit

---
+++ TimerFun/TimerFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimerFun
+//
+//  Created by Thomas Cowern New on 2/10/20.
+//  Copyright © 2020 Thomas Cowern New. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
